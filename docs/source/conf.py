c636cf23a482f12c86d96a163088a6ececdfbb99
Sarah Kaiser <sckaiser@sckaiser.com>
2021-02-05 01:46:26 +0000

    Testing out how this theme would look

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -321,12 +321,30 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "pydata_sphinx_theme"  # 'alabaster', 'sphinx_rtd_theme'
+html_theme = "sphinx_book_theme"
+# "pydata_sphinx_theme"  # 'alabaster', 'sphinx_rtd_theme'
 
+html_theme_options = {
+    "repository_url": "https://github.com/unitaryfund/mitiq",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "path_to_docs": "docs/source/",
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "colab_url": "https://colab.research.google.com/",
+        "notebook_interface": "jupyterlab"
+    },
+    "use_download_button": True,
+}
+
+extra_navbar = "<p>Because evolution is <a href='https://en.wikipedia.org/wiki/Unitarity_(physics)'>unitary</a>.</p>"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+#html_static_path = ['_static']
+#html_css_files = ["uf.css"]
 
 # display logo on top-left of html pages
 html_logo = "img/unitary_fund_logo.png"
+html_favicon = "_static/logo.png"
+html_title = "Mitiq Documentation"
